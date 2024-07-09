ff9b276f277cf240cc596d0b6b05a174efe25cae
thukhakyawe.mcc@gmail.com <thukhakyawe.mcc@gmail.com>
2024-07-09 13:00:57 +0630

    update code

--- main.tf
+++ main.tf
@@ -2,35 +2,88 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "cidr_blocks" {
-  description = "cidr blocks for vpc and subnets"
-  type = list(string)
-}
+variable "vpc_cidr_block" {}
+
+variable "subnet_cidr_block" {}
+
+variable "avail_zone" {}
+
+variable "env_prefix" {}
+
+variable "my_ip" {}
 
 
-
-
-resource "aws_vpc" "development-vpc" {
-  cidr_block = var.cidr_blocks[0]
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
   tags = {
-    Name : "development"
+    Name : "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id            = aws_vpc.development-vpc.id
-  cidr_block        = var.cidr_blocks[1]
-  availability_zone = "us-east-1a"
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.avail_zone
   tags = {
-    Name : "subnet-1-dev"
+    Name : "${var.env_prefix}-subnet-1"
   }
 }
 
-output "dev-vpc-id" {
-  value = aws_vpc.development-vpc.id
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name : "${var.env_prefix}-rtb"
+  }
 }
 
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name : "${var.env_prefix}-igw"
+  }
 }
 
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port  = 22
+    to_port    = 22
+    protocol   = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port  = 8080
+    to_port    = 8080
+    protocol   = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    prefix_list_ids = []
+
+  }
+  tags = {
+    Name : "${var.env_prefix}-sg"
+  }
+
+}
+
+resource "aws_instance" "myapp-server" {
+  ami = ami-06c68f701d8090592
+}
