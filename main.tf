d7557d5d591dd9c1646fd62f4aac5c9a1a64868e
thukhakyawe.mcc@gmail.com <thukhakyawe.mcc@gmail.com>
2024-07-13 21:02:46 +0630

    add backend as S3 for terraform state file

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "tkk-bucket-terraform"
+    key = "myapp/state.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
