fc2a85651e97a2253f374fc5aee6ce47f7124265
thukhakyawe.mcc@gmail.com <thukhakyawe.mcc@gmail.com>
2024-07-13 10:57:36 +0630

    add subnet module

--- main.tf
+++ main.tf
@@ -2,13 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
-variable "my_ip" {}
-variable "instance_type" {}
-variable "public_key_location" {}
+
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -17,39 +11,14 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-  tags = {
-    Name = "${var.env_prefix}-subnet-1"
-  }
-}
-
-resource "aws_internet_gateway" "myapp-igw" {
+module "myapp-subnet" {
+  source           = "./modules/subnet"
+  subnet_cidr_block = var.subnet_cidr_block 
+  avail_zone = var.avail_zone
+  env_prefix = var.env_prefix
   vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name = "${var.env_prefix}-igw"
-  }
 }
 
-resource "aws_route_table" "myapp-route-table" {
-  vpc_id = aws_vpc.myapp-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-
-  tags = {
-    Name = "${var.env_prefix}-rtb"
-  }
-}
-
-resource "aws_route_table_association" "a-rtb-subnet" {
-  subnet_id      = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_route_table.myapp-route-table.id
-}
 
 resource "aws_security_group" "myapp-sg" {
   name   = "myapp-sg"
@@ -105,7 +74,7 @@ resource "aws_key_pair" "ssh-key" {
 resource "aws_instance" "myapp-server" {
   ami                         = data.aws_ami.latest-amazon-linux-image.id
   instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  subnet_id                   = module.myapp-subnet.subnet.id
   vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
   availability_zone           = var.avail_zone
   associate_public_ip_address = true
@@ -120,10 +89,3 @@ data "aws_instance" "myapp-server" {
   instance_id = aws_instance.myapp-server.id
 }
 
-output "aws_ami_id" {
-  value = data.aws_ami.latest-amazon-linux-image.id
-}
-
-output "ec2_public_ip" {
-  value = data.aws_instance.myapp-server.public_ip
-}
